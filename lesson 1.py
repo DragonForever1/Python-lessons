0f300167916026d0480538d3969e6dda0d356320
DragonForever1 <104247202+DragonForever1@users.noreply.github.com>
2026-09-10 17:20:07 +0300

    Update lesson 1.py

--- lesson 1.py
+++ lesson 1.py
@@ -16,3 +16,35 @@ while del > 1:
 
 if total == 0:
     print ("no")
+
+#2
+
+a = int (input())
+b = int (input())
+c = int (input())
+
+def nod(x, y):
+    while y > 0:
+        ost = x % y
+        x = y
+        y = ost
+    return x
+
+three_k = 0
+max_ar = 0
+
+while c != 0:
+    if nod(a, b) < nod(b, c):
+        three_k = three_k + 1
+        ar = (a + b + c) / 3 
+        if ar > max_ar:
+            max_ar = ar  
+            
+    a = b
+    b = c
+    c = int (input())
+
+if three_k > 0:
+    print (three_k, round(max_ar, 2))
+else:
+    print ("no")
